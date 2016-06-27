673d0d55c2a9772f3de9d3346c2793105e5bcb5b
katoch2011 <gupta.ashish5610@gmail.com>
2016-06-28 01:55:14 +0530

    Added Extentions

--- app/src/main/java/ashishgupta/com/scarnes_dice/MainActivity.java
+++ app/src/main/java/ashishgupta/com/scarnes_dice/MainActivity.java
@@ -2,6 +2,7 @@ package ashishgupta.com.scarnes_dice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,7 +12,7 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView dice;
+    ImageView dice,dice2;
     Button hold,roll,reset;
     int userOverallScore,computerTurnScore,computerOverallScore;
     TextView userRound,userOverall,computerRound,computerOverall;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         dice=(ImageView)findViewById(R.id.Dice);
         dice.setImageResource(R.drawable.one);
+        dice2=(ImageView)findViewById(R.id.Dice2);
+        dice2.setImageResource(R.drawable.one);
         hold=(Button)findViewById(R.id.hold);
         roll=(Button)findViewById(R.id.roll);
         reset=(Button)findViewById(R.id.roll);
@@ -44,45 +47,71 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void roll()
-    {
+    {   hold.setEnabled(true);
         int i=new Random().nextInt(6)+1;
-        switch (i)
+        int k=new Random().nextInt(6)+1;
+        switchCase(i,dice); switchCase(k,dice2);
+
+        if(i==1 && k==1)
         {
-            case 1: dice.setImageResource(R.drawable.one);
-                    userTurnScore=0;
-                    userRound.setText("0");
-                    Toast.makeText(this,"Lolz....hit a 0",Toast.LENGTH_SHORT).show();
-                    computerTurn();
-                    break;
-
-            case 2: dice.setImageResource(R.drawable.two);
-                    userTurnScore +=2;
-                    userRound.setText(String.valueOf(userTurnScore));
-                     break;
-            case 3:dice.setImageResource(R.drawable.three);
-                     userTurnScore +=3;
-                     userRound.setText(String.valueOf(userTurnScore));
-                break;
-            case 4:  dice.setImageResource(R.drawable.four);
-                    userTurnScore +=4;
-                    userRound.setText(String.valueOf(userTurnScore));
-                break;
-            case 5:  dice.setImageResource(R.drawable.five);
-                      userTurnScore +=5;
-                      userRound.setText(String.valueOf(userTurnScore));
-                break;
-            case 6:   dice.setImageResource(R.drawable.six);
-                      userTurnScore +=6;
-                userRound.setText(String.valueOf(userTurnScore));
-
+            Toast.makeText(this,"User ... Doomed ...",Toast.LENGTH_SHORT).show();
+            userOverallScore=0;
+            userTurnScore=0;
+            userRound.setText(String.valueOf(userTurnScore));
+            userOverall.setText("0");
+            Log.e("VAlue of i and k ",String.valueOf(i)+" , "+String.valueOf(k));
+            computerTurn();
+        }
+        else if(i==1 || k==1)
+        {  Toast.makeText(this,"user hit 0",Toast.LENGTH_SHORT).show();
+            userTurnScore=0;
+            userRound.setText(String.valueOf("0"));
+            Log.e("VAlue of i and k ",String.valueOf(i)+" , "+String.valueOf(k));
+            computerTurn();
+        }
+        else if (i==k)
+        {
+            Log.e("VAlue of i and k ",String.valueOf(i)+" , "+String.valueOf(k));
+            Toast.makeText(this,"Double hit ,press roll again",Toast.LENGTH_SHORT).show();
+            hold.setEnabled(false);
+        }
+        else
+        {
+            Log.e("VAlue of i and k ",String.valueOf(i)+" , "+String.valueOf(k));
+            Log.e("user score",String.valueOf(userTurnScore));
+            userRound.setText(String.valueOf(userTurnScore));
         }
 
+    }
+    public void switchCase(int i,ImageView temp)
+    {
+        switch (i)
+        {
+            case 1: temp.setImageResource(R.drawable.one);
+                    userTurnScore=0;
+                     break;
+            case 2: temp.setImageResource(R.drawable.two);
+                    userTurnScore +=2;
+                     break;
+            case 3:temp.setImageResource(R.drawable.three);
+                    userTurnScore +=3;
+                    break;
+            case 4:  temp.setImageResource(R.drawable.four);
+                    userTurnScore +=4;
+                   break;
+            case 5:  temp.setImageResource(R.drawable.five);
+                    userTurnScore +=5;
+                break;
+            case 6:   temp.setImageResource(R.drawable.six);
+                     userTurnScore +=6;
+        }
     }
 
     public void hold()
     {
            userOverallScore+=userTurnScore;
            userOverall.setText(String.valueOf(userOverallScore));
+           Log.e("User overall score",String.valueOf(userOverallScore));
            userTurnScore=0;
            userRound.setText("0");
           if(userOverallScore>100)
@@ -104,6 +133,9 @@ public class MainActivity extends AppCompatActivity {
         computerRound.setText("0");
         hold.setEnabled(true);
         roll.setEnabled(true);
+        Toast.makeText(this,"Game Reset ",Toast.LENGTH_SHORT).show();
+        dice.setImageResource(R.drawable.one);
+        dice2.setImageResource(R.drawable.one);
 
     }
 
@@ -111,18 +143,29 @@ public class MainActivity extends AppCompatActivity {
     {
         Toast.makeText(this,"Computers turn",Toast.LENGTH_SHORT).show();
         computerTurnScore=0;
-        int i;
+        int i,k;
         computerRound.setText("0");
-        while(computerTurnScore<20)
+        while(computerTurnScore<25)
         {
 
             i=new Random().nextInt(6)+1;
-            if(i==1)
+            k=new Random().nextInt(6)+1;
+            Log.e("Computer  i and k ",String.valueOf(i)+" , "+String.valueOf(k));
+            if(i==1 && k==1)
             {
-             computerTurnScore=0;
-                computerRound.setText("0");
-            }else {
-                computerTurnScore += i;
+                Toast.makeText(this,"Computer  Doomed ...",Toast.LENGTH_SHORT).show();
+                computerOverallScore=0;
+                computerTurnScore=0;
+                computerRound.setText(String.valueOf(computerTurnScore));
+                break;
+            }
+            else if(i==1 || k==1)
+            {  Toast.makeText(this,"computer hit 0 ",Toast.LENGTH_SHORT).show();
+                computerRound.setText(String.valueOf(computerTurnScore));
+                break;
+            }
+            else
+            { computerTurnScore+=i+k;
                 computerRound.setText(String.valueOf(computerTurnScore));
             }
 
